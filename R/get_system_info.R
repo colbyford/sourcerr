84520463e27770b092a1ff2b4ebeea055e7d6eb3
Colby Ford <colbytylerford@gmail.com>
2019-02-09 13:24:36 -0500

    Create get_system_info.R

---
+++ R/get_system_info.R
@@ -0,0 +1,11 @@
+
+get_system_info <- function(format = "list"){
+  #sysinf <- as.data.frame(Sys.info())
+  #memorysize <- memory.size()
+  systeminfo <- list("R Version" = paste(sessioninfo[["R.version"]][["version.string"]],
+                                               sessioninfo[["R.version"]][["nickname"]]),
+                           "System Version" = sessioninfo[["running"]],
+                           "Number of Cores" = parallel::detectCores())
+
+  return(systeminfo)
+}
