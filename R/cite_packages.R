fd78a9deef2d2a799f0274baf689d154e7c3cca6
Colby Ford <colbytylerford@gmail.com>
2019-03-02 11:38:24 -0500

    Add package list functionality

--- R/cite_packages.R
+++ R/cite_packages.R
@@ -1,29 +1,34 @@
 #' @title Generate citation list of all packages in a variety of formats.
 #'
+#' @param packages (Optional) Vector of package names. If blank, the package will look to loaded packages to be included in the citation list.
 #' @param outfile Path and filename where the citation file should be written.
 #' @param format Output format of the citation file. Select from "bibtex", "latex", "html", or "text".
 #' @param detaillevel Packages to be included in the output.
 #' Select "loaded" for all loaded packages, "associated" for all loaded packages + associated packages, or "all" to include base packages and all other packages in the session.
 #' @export cite_packages
 
-cite_packages <- function(outfile, format = "bibtex", detaillevel = "loaded"){
+cite_packages <- function(packages = c(), outfile, format = "bibtex", detaillevel = "loaded"){
   ## Capture current session information
   sessioninfo <- sessionInfo()
 
-  if (detaillevel == "loaded"){
-    ## Get a list of all the packages that have been loaded
-    packages <- names(sessioninfo$otherPkgs)
+  if (length(packages) > 0){
+    ## Do nothing as input package list will be used
+  } else{
+    if (detaillevel == "loaded"){
+      ## Get a list of all the packages that have been loaded
+      packages <- names(sessioninfo$otherPkgs)
 
-  } else if (detaillevel == "associated"){
-    ## Get a list of all the packages that have been loaded + all associated packages
-    packages <- unique(c(names(sessioninfo$otherPkgs),
-                         names(sessioninfo$loadedOnly)))
+    } else if (detaillevel == "associated"){
+      ## Get a list of all the packages that have been loaded + all associated packages
+      packages <- unique(c(names(sessioninfo$otherPkgs),
+                           names(sessioninfo$loadedOnly)))
 
-  } else if (detaillevel == "all"){
-    ## Get a list of all the packages that have been loaded + all associated packages + base packages
-    packages <- unique(c(names(sessioninfo$basePkgs),
-                         names(sessioninfo$otherPkgs),
-                         names(sessioninfo$loadedOnly)))
+    } else if (detaillevel == "all"){
+      ## Get a list of all the packages that have been loaded + all associated packages + base packages
+      packages <- unique(c(names(sessioninfo$basePkgs),
+                           names(sessioninfo$otherPkgs),
+                           names(sessioninfo$loadedOnly)))
+    }
   }
 
   ## Remove `sourcerr` from packages list
@@ -34,6 +39,7 @@ cite_packages <- function(outfile, format = "bibtex", detaillevel = "loaded"){
     for (i in 1:length(packages)){
       package <- packages[i]
       citation <- citation(package = package)
+
       if (format == "text"){
         print(citation, style = "text")
       } else if (format == "bibtex"){
@@ -43,7 +49,6 @@ cite_packages <- function(outfile, format = "bibtex", detaillevel = "loaded"){
       } else if (format == "latex"){
         print(citation, style = "latex")
       }
-      print("\n")
     },
     file = outfile) ## Write file to outfile parameter
 
--- test/test.R
+++ test/test.R
@@ -9,3 +9,16 @@ cite_packages(outfile = "../../Downloads/citations.bibtex",
               format = "bibtex",
               detaillevel = "loaded")
 
+## Cite a list of packages
+packages <- c("dplyr",
+              "ggplot2",
+              "tidyr",
+              "doParallel")
+
+cite_packages(packages = packages,
+              format = "html",
+              outfile = "test/test_output.html")
+
+cite_packages(packages = packages,
+              format = "latex",
+              outfile = "test/test_output.tex")
