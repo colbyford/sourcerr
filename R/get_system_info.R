e2ff1ae9ffbf69cf16b6ae50a1c077cffd03c1e3
Colby Ford <colbytylerford@gmail.com>
2020-07-28 09:38:03 -0400

    Update get_system_info.R

--- R/get_system_info.R
+++ R/get_system_info.R
@@ -2,6 +2,9 @@
 get_system_info <- function(format = "list"){
   #sysinf <- as.data.frame(Sys.info())
   #memorysize <- memory.size()
+
+  sessioninfo <- sessionInfo()
+
   systeminfo <- list("R Version" = paste(sessioninfo[["R.version"]][["version.string"]],
                                                sessioninfo[["R.version"]][["nickname"]]),
                            "System Version" = sessioninfo[["running"]],
